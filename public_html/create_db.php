9bfeac2db180cb290bda1d431227f47919375837
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2011-11-17 11:06:31 +0100

    modified:   create_db.php 	modified:   register.php

--- public_html/create_db.php
+++ public_html/create_db.php
@@ -15,7 +15,7 @@
     if (!$db)
         mysqli_connect_error();
 
-    printf("Success...%s<br />", mysqli_get_host_info($db));
+    printf("Success...%s\n<br />\n", mysqli_get_host_info($db));
     
     // set autocommit
     mysqli_autocommit($db, TRUE);
@@ -24,7 +24,7 @@
     $sql = "CREATE DATABASE IF NOT EXISTS ".$db_name;
     if (!mysqli_query($db, $sql))
         printf("Error: %s\n", mysqli_error($db));
-    printf("DB sucessfully created.<br />");
+    printf("DB sucessfully created.\n<br />\n");
     
     // set default DB name
     mysqli_select_db($db, $db_name);
@@ -32,11 +32,13 @@
     // create TABLE (recipes)
     $sql = "CREATE TABLE IF NOT EXISTS ".$table." (
          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-         data VARCHAR(100)
+         title VARCHAR(100),
+         ingredients TEXT,
+         preparation TEXT
     )";
     if (!mysqli_query($db, $sql))
         printf("Error: %s\n", mysqli_error($db));
-    printf("TABLE succesfully created.<br />");
+    printf("TABLE succesfully created.\n<br />\n");
 
     // read data from file
     $file = "./data/Recettes.xml";
@@ -48,28 +50,48 @@
         // TODO: don't delete the '
         // TODO: error handling
         return utf8_encode(preg_replace('[\']', null, $str));
+        // global $db;
+        // return utf8_encode(mysqli_real_escape_string($db, $str));
     }
-    
+
     $index = 0;
     while (($line = fgets($fp)) && ($index = $index + 1)) { // $i++ ?!
         $line = clean_line($line);
         $data_array[$index] = simplexml_load_string($line);
     }
     fclose($fp);
-    
-    unset ($index);
-    //print_r($data_array[1]);
+    /*echo "<pre>";
+    print_r($data_array); 
+    echo "</pre>";
+    unset ($index);*/
+
 
     // submit to DB
     // TODO: add data (ingredients etc)
     $sql = "";
-    foreach($data_array as $recipe) {
-        // TODO: id = NULL => id = index
-        $sql .= "INSERT INTO $table VALUES ('NULL', '$recipe->TI');";
+    $ing = "";
+    foreach($data_array as $index => $recipe) {
+        // TODO: Error handling (multiple executions)
+        foreach($recipe->IN as $ingredient)
+            $ing .= $ingredient->ING."--";
+        //echo $ing."<br/>";
+        $sql = "INSERT INTO $table VALUES ('$index', '$recipe->TI', '$recipe->PR', '$ing')";
+
+        if (!mysqli_query($db, $sql))
+            printf("Error: %s\n<br />\n", mysqli_error($db));
+
+        $ing = "";
     }
-    if (!mysqli_multi_query($db, $sql))
-        printf("Error: %s\n",mysqli_error($db));
-    printf("Database succesfully filled\n");
+/*    if (!mysqli_multi_query($db, $sql))
+        printf("Error: %s\n<br />\n",mysqli_error($db));
+    printf("Database succesfully filled\n");*/
+
+    // test if already present
+    // $sql = "SELECT * FROM ".$table." WHERE id = 1";
+    
+    // search
+    // $sql = "SELECT `preparation` FROM ".$table." WHERE ingredients like '%orange%'";
+
 
     mysqli_close($db);
 ?>
