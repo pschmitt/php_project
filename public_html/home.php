f531b4c30773a97f468b8bb5415cb75a1fd88d15
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2011-11-17 11:07:56 +0100

    modified:   home.php

--- public_html/home.php
+++ public_html/home.php
@@ -5,7 +5,7 @@
 <div class="entry-content">
     <div class="style">
         <img width="300" height="168" alt="" src="images/turkey-300x168.jpg" title="turkey" class="alignleft">
-        <p>PDPraesent vestibulum aenean Nonummyhendrerit mauris. Hasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Nulla dui.&nbsp; etur ridiculus mus. Nulla dui.<span id="more-74"></span></p>
+        <p>Praesent vestibulum aenean Nonummyhendrerit mauris. Hasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Nulla dui.&nbsp; etur ridiculus mus. Nulla dui.<span id="more-74"></span></p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel  turpis sed justo placerat faucibus. Vestibulum ante ipsum primis in  faucibus orci luctus et ultrices posuere cubilia Curae; Quisque  imperdiet posuere neque non interdum. Nulla ut ultrices justo.  Suspendisse euismod auctor tincidunt. Duis in nisl sit amet eros varius  placerat sit amet ut erat. Quisque fringilla ultricies elementum.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel  turpis sed justo placerat faucibus. Vestibulum ante ipsum primis in  faucibus orci luctus et ultrices posuere cubilia Curae; Quisque  imperdiet posuere neque non interdum. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Nulla dui.&nbsp; etur ridiculus mus. Nulla dui.</p>
     </div>
