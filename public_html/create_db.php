0c9634f6f5a2ae72b9faf6faf37719a0a0e38f69
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2011-11-21 00:59:53 +0100

    UI improvements (create_db.php)

--- public_html/create_db.php
+++ public_html/create_db.php
@@ -5,18 +5,20 @@
 </head>
 	
 <body>
-
+<pre>
 <?php
+    // TODO Error handling (query or die ?)
     $passwd_file = realpath('../.login/DB_credentials.php');
     require "$passwd_file";
-    
+   
+    define("INGREDIENT_DELIMITER", "--");
+
     // create database
-    $db = mysqli_connect($db_host, $db_user, $db_password);
-    if (!$db)
+    if (!$db = mysqli_connect($db_host, $db_user, $db_password))
         mysqli_connect_error();
 
-    printf("Success...%s\n<br />\n", mysqli_get_host_info($db));
-    
+    printf("Success... %s\n", mysqli_get_host_info($db));
+
     // set autocommit
     mysqli_autocommit($db, TRUE);
 
@@ -24,12 +26,13 @@
     $sql = "CREATE DATABASE IF NOT EXISTS ".$db_name;
     if (!mysqli_query($db, $sql))
         printf("Error: %s\n", mysqli_error($db));
-    printf("DB sucessfully created.\n<br />\n");
+    printf("DB sucessfully created.\n");
     
     // set default DB name
     mysqli_select_db($db, $db_name);
 
     // create TABLE (recipes)
+    // TODO id: unsigned int ; max_size for entries
     $sql = "CREATE TABLE IF NOT EXISTS ".$table." (
          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
          title VARCHAR(100),
@@ -38,7 +41,14 @@
     )";
     if (!mysqli_query($db, $sql))
         printf("Error: %s\n", mysqli_error($db));
-    printf("TABLE succesfully created.\n<br />\n");
+    printf("TABLE succesfully created.\n");
+
+    $sql = "SELECT * FROM ".$table;
+    if (mysqli_num_rows(mysqli_query($db, $sql)) != 0) {
+        printf('You just ran the script man ... What \'bout <a href="erase_db.php">truncating|erasing</a> first ?');
+        exit;
+    }
+    unset($sql);
 
     // read data from file
     $file = "./data/Recettes.xml";
@@ -47,11 +57,21 @@
         die("impossible d'ouvrir le fichier xml");
     
     function clean_line($str) {
-        // TODO: don't delete the '
-        // TODO: error handling
-        return utf8_encode(preg_replace('[\']', null, $str));
-        // global $db;
-        // return utf8_encode(mysqli_real_escape_string($db, $str));
+        // TODO don't delete the '
+        // TODO error handling
+        //return utf8_encode(preg_replace('[\']', null, $str));
+        return utf8_encode(preg_replace('[\']','\\\'' , $str));
+        //global $db;
+        //echo "db = ".$db."\n";
+        //return mysqli_real_escape_string($db, $str);
+    }
+
+    function submit($db, $sql) {
+        if ((!isset($db, $sql)) || (!mysqli_query($db, $sql))) {
+            printf("Error: %s\n", mysqli_error($db));
+            return 1;
+        } else
+            return 0;
     }
 
     $index = 0;
@@ -60,31 +80,32 @@
         $data_array[$index] = simplexml_load_string($line);
     }
     fclose($fp);
-    /*echo "<pre>";
-    print_r($data_array); 
-    echo "</pre>";
-    unset ($index);*/
-
-
-    // submit to DB
-    // TODO: add data (ingredients etc)
-    $sql = "";
-    $ing = "";
+    
+    /*
+        // TEST
+        print_r($data_array); 
+        unset ($index);
+    */
+    
+    printf("Parsing XML data and inserting to DB");
     foreach($data_array as $index => $recipe) {
+        printf("Processing line %s ...", $index); 
+        $ing = NULL;
         // TODO: Error handling (multiple executions)
         foreach($recipe->IN as $ingredient)
-            $ing .= $ingredient->ING."--";
-        //echo $ing."<br/>";
-        $sql = "INSERT INTO $table VALUES ('$index', '$recipe->TI', '$recipe->PR', '$ing')";
+            $ing .= $ingredient->ING.INGREDIENT_DELIMITER;
+        $sql = "INSERT INTO $table VALUES ('".$index."', '".$recipe->TI."', '".$recipe->PR."', '".$ing."')";
+        
+        submit($db, $sql);
 
-        if (!mysqli_query($db, $sql))
-            printf("Error: %s\n<br />\n", mysqli_error($db));
-
-        $ing = "";
+        printf("done\n");
     }
-/*    if (!mysqli_multi_query($db, $sql))
-        printf("Error: %s\n<br />\n",mysqli_error($db));
-    printf("Database succesfully filled\n");*/
+
+    /*
+    if (!mysqli_multi_query($db, $sql))
+        printf("Error: %s\n",mysqli_error($db));
+    printf("Database succesfully filled\n");
+    */
 
     // test if already present
     // $sql = "SELECT * FROM ".$table." WHERE id = 1";
@@ -95,6 +116,6 @@
 
     mysqli_close($db);
 ?>
-
+</pre>
 </body>
 </html>
--- public_html/erase_db.php
+++ public_html/erase_db.php
@@ -5,18 +5,38 @@
 </head>
 
 <body>
-<?php
-    $passwd_file = realpath('../.login/DB_credentials.php');
-    require "$passwd_file";
-    
-    $db = mysqli_connect($db_host, $db_user, $db_password, $db_name);
-    
-    $sql = "DROP DATABASE ".$db_name;
-    if (!mysqli_query($db, $sql))
-        printf("Error: %s\n", mysqli_error($db));
-    printf("Database successfully removed");
-    mysqli_close($db);
-?>
+<pre>
+    <?php
+        $passwd_file = realpath('../.login/DB_credentials.php');
+        require "$passwd_file";
+        
+        function usage() {
+            printf("Required parameter: o\n-d: delete database(%s)\n-t: truncate table (%s)", $GLOBALS['db_name'], $GLOBALS['table']);
+            exit;
+        }
 
+        if (!isset($_GET['o']))
+            usage();
+        
+        $db = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+        
+        
+        switch($_GET['o']) {
+            case 't':
+                $sql = "TRUNCATE TABLE ".$table;
+                $msg = "Truncated";
+                break;
+            case 'd':
+                $sql = "DROP DATABASE ".$db_name;
+                $msg = "Deleted";
+                break;
+        }
+
+        if ((isset($sql)) && (! mysqli_query($db, $sql)))
+            printf("Error: %s\n", mysqli_error($db));
+        printf("Done: %s\n", $msg);
+        mysqli_close($db);
+    ?>
+</pre>
 </body>
 </html>
