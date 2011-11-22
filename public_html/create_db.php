acc1661b13ae0f17017d4f7178e80117b6e30435
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2011-11-22 13:37:17 +0100

    speedup

--- public_html/create_db.php
+++ public_html/create_db.php
@@ -9,8 +9,11 @@
 <?php
     // TODO Error handling (query or die ?)
     $passwd_file = realpath('../.login/DB_credentials.php');
+    $file = "./data/Recettes.xml";
     require "$passwd_file";
-   
+    
+    
+
     define("INGREDIENT_DELIMITER", "--");
 
     // create database
@@ -50,40 +53,59 @@
         or die('You just ran the script man ... What \'bout <a href="erase_db.php">truncating|erasing</a> first ?');
     unset($sql);
         
-    function clean_line($str) {
+    function clean_line($str, $db) {
         // TODO don't delete the '
         // TODO error handling
         //return utf8_encode(preg_replace('[\']', null, $str));
         return utf8_encode(preg_replace('[\']','\\\'' , $str));
-        /*global $db;
-        echo "db = ".$db."\n";
-        return mysqli_real_escape_string($db, $str);*/
+        //global $db;
+        //echo "db = ".$db."\n";
+        return mysqli_real_escape_string($db, utf8_encode($str));
     }
 
     function submit($db, $sql) {
         if ((!isset($db, $sql)) || (!mysqli_query($db, $sql))) {
-            printf("Error: %s\n", mysqli_error($db));
-            return 0;
-        } else
-            return 1;
+            die("Error: %s\n", mysqli_error($db));
     }
-   
-    function line_to_db($line, $db, $table) {
+    
+    //TODO INSERT INTO VALUES ("", "", ""), ("", "", "")
+    function get_values($line, $db) {
         isset($line)
             or die("No param. Exiting.\n");
-        $line = clean_line($line);
+        $line = clean_line($line, $db);
         $sxml = simplexml_load_string($line);
         $ing = NULL;
         foreach($sxml->IN as $ingredient)
             $ing .= $ingredient->ING.INGREDIENT_DELIMITER;
-        $sql = "INSERT INTO ".$table." VALUES ('NULL', '".$sxml->TI."', '".$sxml->PR."', '".$ing."')";
-        submit($db, $sql)
-            or die("Couldn't submit !\n");
+        
+        return "('NULL', '".$sxml->TI."', '".$sxml->PR."', '".$ing."'),";
     }
 
     printf("Parsing XML data and inserting to DB\n");
-    $file = "./data/Recettes.xml";
+    function parse_file ($file, $db, $table) {
+        // read data from file
+        $fp = fopen($file, "r");
+        if (!$fp)
+            die("Couldn't open xml file\n");
+        $sql = "INSERT INTO ".$table." VALUES ";
+        $index = 0;
+        while (($line = fgets($fp)) && ($index = $index + 1)) { // $i++ ?!
+            printf("Processing line %s ...", $index);
+            if (!($index % 100)) {
+                // submit and reset $sql
+                submit($db, substr($sql, 0, -1)) or die();
+                $sql = "INSERT INTO ".$table." VALUES ";
+            }
+            $values = get_values($line, $db) ;
+            $sql .= $values;
+            printf("done\n");
+        }
+        submit($db, substr($sql, 0, -1)) or die();
+        fclose($fp)
+            or die("Couldn't close file");
+    }
     parse_file($file, $db, $table);
+    
     // search
     // $sql = "SELECT `preparation` FROM ".$table." WHERE ingredients like '%orange%'";
 
