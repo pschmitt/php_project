7de2177816caa6bfd2614d875cf867dc568672bc
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2011-12-06 00:34:01 +0100

    modified:   .gitignore 	modified:   .login/DB_credentials.php 	modified:   README.pod

--- .login/DB_credentials.php
+++ .login/DB_credentials.php
@@ -1,17 +1,22 @@
 <?php
-	$hostname="Math-Portable"; // MAY BE CHANGED TO SUIT YOUR HOSTNAME
-	$server = $_SERVER['SERVER_NAME'];
-	$table = "Recipes";
+	$hostname = "LaXLinux-CL"; // MAY BE CHANGED TO SUIT YOUR HOSTNAME
+	$server   = $_SERVER['SERVER_NAME'];
+	$table    = "Recipes";
 
-	if (($server === "localhost") || ($server === "127.0.0.1") || ($server === $hostname)){
+	if (($server === "localhost") || ($server === "127.0.0.1") || ($server === $hostname)) {
 		$db_host = "localhost";
-		$db_name = "cooking";
-		$db_user = "root";
-		$db_password = "";	
+		$db_name = "JeNeSaisPasCuisiner";
+		$db_user = "silly";
+		$db_password = "none";	
 	} else {
 		$db_host = "XXXX";
 		$db_name = "XXXX";
 		$db_user = "XXXX";
 		$db_password = "XXXX";
 	}
+    unset($server, $hostname);
+
+    // credentials array, useful for passing to functions
+
+    $credentials = array("db_host" => $db_host, "db_name" => $db_name, "db_user" => $db_user, "db_password" => $db_password, "table" => $table);
 ?>
