2a4697dea56e2a0432123c125ea9c6c747e6f37e
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2011-12-12 21:17:56 +0100

    modified:   queries.inc.php

--- public_html/includes/functions/queries.inc.php
+++ public_html/includes/functions/queries.inc.php
@@ -11,7 +11,17 @@
                       .$tables["Ingredients"]." AS i
                 WHERE R.id=ln.id_recipe
                 AND ln.id_ingredient=i.id
-                AND i.name='".mysqli_real_escape_string($db, $ingredient)."'";
+                AND i.name LIKE '%".mysqli_real_escape_string($db, $ingredient)."%'";
     }
+    
+    function recipe_by_title($title) {
+        if (!isset($GLOBALS['db'], $GLOBALS['tables']))
+            die("No DB connection !");
+        $db = $GLOBALS['db'];
+        $tables = $GLOBALS['tables'];
 
+        return "SELECT title
+                FROM ".$tables["Recipes"].
+                " WHERE title LIKE '%".mysqli_real_escape_string($db, $title)."%'";
+    }
 ?>
