3d07a5f7fac02fd42f31929592e79a09fb274dff
Math <mathieumorainville@hotmail.com>
2011-12-14 10:11:43 +0100

    Gestion de l'affichage des recettes via thesaurus : ok

--- public_html/recipes.php
+++ public_html/recipes.php
@@ -10,19 +10,26 @@ if (isset($_GET['recipe_id'])) {
 	
 	require_once("includes/functions/mysqli.inc.php");
 	$db = db_con();
-	
+	$ing_list = array();
 	if (isset($Thesaurus[$_GET['cat']])) {
 		// Construction de la requête SQL
-		$ing_list = ''; // Liste d'ingrédients
+		// Liste d'ingrédients
 		
 		function make_ing_list($ingredient_type, $array) {
 			global $db;
 			global $ing_list;
 			
 			if (isset($ingredient_type['S'])) {
+				foreach($ingredient_type['E'] as $synonyms => $synonym) {
+					$str = mysqli_real_escape_string($db, $synonym);
+					if (!in_array($str, $ing_list))
+						$ing_list[] = $str;
+				}
 				foreach($ingredient_type['S'] as $sons => $son) {
 					foreach($array[$son]['E'] as $synonyms => $synonym) {
-						$ing_list .= "'".mysqli_real_escape_string($db, $synonym)."', ";
+						$str = mysqli_real_escape_string($db, $synonym);
+						if (!in_array($str, $ing_list))
+							$ing_list[] = $str;
 					}
 					//echo $array[$son]['T'].", ";
 					make_ing_list($array[$son], $array);
@@ -30,21 +37,23 @@ if (isset($_GET['recipe_id'])) {
 			}
 		}
 		
-		make_ing_list($Thesaurus[$_GET['cat']-1], $Thesaurus);
-		
-		$ing_list = substr($ing_list, 0, -2); // On enlève la virgule et l'espace après le dernier ingrédient
+		make_ing_list($Thesaurus[$_GET['cat']], $Thesaurus);
+		$ing_list = implode("', '", $ing_list);
+		//$ing_list = substr($ing_list, 0, -2); // On enlève la virgule et l'espace après le dernier ingrédient
 		//echo $ing_list;
 		
-		$sql = "SELECT DISTINCT title
+		$sql = "SELECT DISTINCT *
 				FROM Recipes AS R, Recipes_ln_Ingredients AS ln, Ingredients AS i
-				WHERE R.id=ln.id_recipe AND ln.id_ingredient=i.id AND i.name IN (".$ing_list.")";
+				WHERE R.id=ln.id_recipe AND ln.id_ingredient=i.id AND i.name IN ('".$ing_list."')";
 		
 		echo $sql;
 		
 		$result = mysqli_query ($db, $sql) or die (mysqli_error($db));
 		
 		while ($row = mysqli_fetch_assoc($result)) {
-			//print_r($row);
+			echo "<br /><br /><strong>Titre de la recette</strong><br />\n";
+			//print_r($row)
+			echo $row['title'];;
 		}
 	}
 }
