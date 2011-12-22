7fe3e68e55f09b2ae477cf54b03783cfc6227936
Philipp Schmitt <philipp.schmitt@laxlinux.net>
2011-12-22 23:50:24 +0100

    Et hop ! suppression des favoris dans favs.php. Vive jQuery !

--- public_html/favs.php
+++ public_html/favs.php
@@ -17,8 +17,12 @@
                     <a href="'.$_SERVER['PHP_SELF'].'?p=recipes&recipe_id=%s">
                         %s
                     </a>
+                    -
+                    <a id="%s" class="rm_bookmark" href="#" >
+                        supprimer
+                    </a>
                 </li>
-                ', $row['id'], $row['title']);
+                ', $row['id'], $row['title'], $row['id']);
     }
 ?>
     </ul>
@@ -27,3 +31,26 @@
     }
 ?>
 
+<script>
+    $(".rm_bookmark").click(function() {
+        $.ajax({
+            type: "POST",
+            url: "includes/functions/rm_bookmark.php",
+            data: { recipe_id: $(this).attr('id') , user_id: <?php echo isset($_SESSION['user_id']) ? $_SESSION['user_id'] : '-1'; ?> },
+            success: function(server_response) {
+                $("#result").ajaxComplete(function(event, request) {
+                    if (server_response == '0') {
+                        $("#result").html(rm_txt);
+                        $("#result").show("fast");
+                        $("#bookmark").attr('src', add_icon);
+                        $("#bookmark").attr('title', add_title);
+                    } else {
+                        $("#result").html(rm_failed_txt);
+                    }
+                    $("#result").fadeOut(3000);
+                });
+            }
+        });
+        $(this).parent().remove();
+    });
+</script>
